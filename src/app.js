bc3110a9a3a22152214ad3e68a02717e9c88956d
Ignacio <talonignacio07@gmail.com>
2024-01-23 16:45:40 -0300

    tercer desafio backend

---
+++ src/app.js
@@ -0,0 +1,88 @@
+//*DESAFIO NUMERO 3*//
+
+const express = require("express");
+const app = express();
+const PUERTO = 8080;
+
+const ProductManager = require("../src/controllers/product-manager.js");
+const productManager = new ProductManager("./src/models/productos.json");
+
+//Middleware
+app.use(express.json());
+//No se olviden!
+
+//Listar todos los productos
+
+app.get("/api/products", async (req, res) => {
+  //Api/products lo pide la pre entrega, no el desafio.
+  try {
+    const limit = req.query.limit;
+
+    //Cargamos el array de productos con getProducts.
+    const productos = await productManager.getProducts();
+
+    //Si hay limite retornamos el limite y sino retornamos todo
+    if (limit) {
+      res.json(productos.slice(0, limit));
+    } else {
+      res.json(productos);
+    }
+  } catch (error) {
+    console.log("Error al obtener los productos", error);
+    res.status(500).json({ error: "Error del servidor" });
+  }
+});
+
+//Traer un solo producto por id:
+
+app.get("/api/products/:pid", async (req, res) => {
+  let id = req.params.pid;
+
+  try {
+    const producto = await productManager.getProductById(parseInt(id));
+    if (!producto) {
+      res.json({
+        error: "El producto no existe",
+      });
+    } else {
+      res.json(producto);
+    }
+  } catch (error) {
+    console.log("Error al obtener el producto", error);
+    res.status(500).json({ error: "Error del servidor" });
+  }
+});
+
+//**AVANCE DE LA PRIMER PRE-ENTREGA**//
+
+//Agregar un nuevo producto por post:
+
+app.post("/api/products", async (req, res) => {
+  const nuevoProducto = req.body;
+  console.log(nuevoProducto);
+
+  try {
+    await productManager.addProduct(nuevoProducto),
+      res.status(201).json({ message: "Producto agregado exitosamente" });
+  } catch (error) {
+    console.log("error al agregar un producto ", error);
+    res.status(500).json({ error: "error del servidor, vamos a morir" });
+  }
+});
+
+//Actualizamos producto por id:
+
+app.put("/api/products/:pid", async (req, res) => {
+  let id = req.params.pid;
+  const productoActualizado = req.body;
+
+  try {
+    await productManager.updateProduct(parseInt(id), productoActualizado);
+    res.json({ message: "Producto actualizado correctamente" });
+  } catch (error) {
+    console.log("No pudimos actualizar, vamos a morir ", error);
+    res.status(500).json({ error: "Error del server" });
+  }
+});
+
+app.listen(PUERTO);
